e44f9256f35ed676e77fbd76bc3656530932082b
kauer3 <kaue0607@gmail.com>
2021-10-11 13:29:38 -0300

    Add ssl

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/framework/src/Env')} */
-const Env = use('Env')
+const Env = use("Env");
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
-const Helpers = use('Helpers')
+const Helpers = use("Helpers");
 
 module.exports = {
   /*
@@ -16,7 +16,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection:  Env.get('DB_CONNECTION', 'pg'),
+  connection: Env.get("DB_CONNECTION", "pg"),
 
   /*
   |--------------------------------------------------------------------------
@@ -30,12 +30,14 @@ module.exports = {
   |
   */
   sqlite: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+      filename: Helpers.databasePath(
+        `${Env.get("DB_DATABASE", "development")}.sqlite`
+      ),
     },
     useNullAsDefault: true,
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get("DB_DEBUG", false),
   },
 
   /*
@@ -49,15 +51,15 @@ module.exports = {
   |
   */
   mysql: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get("DB_HOST", "localhost"),
+      port: Env.get("DB_PORT", ""),
+      user: Env.get("DB_USER", "root"),
+      password: Env.get("DB_PASSWORD", ""),
+      database: Env.get("DB_DATABASE", "adonis"),
     },
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get("DB_DEBUG", false),
   },
 
   /*
@@ -70,15 +72,16 @@ module.exports = {
   | npm i --save pg
   |
   */
-	pg: {
-		client: 'pg',
-		connection: {
-			host: Env.get('DB_HOST', 'localhost'),
-			port: Env.get('DB_PORT', ''),
-			user: Env.get('DB_USER', 'root'),
-			password: Env.get('DB_PASSWORD', ''),
-			database: Env.get('DB_DATABASE', 'adonis'),
-		},
-		debug: Env.get('DB_DEBUG', false),
-	},
-}
+  pg: {
+    client: "pg",
+    connection: {
+      host: Env.get("DB_HOST", "localhost"),
+      port: Env.get("DB_PORT", ""),
+      user: Env.get("DB_USER", "root"),
+      password: Env.get("DB_PASSWORD", ""),
+      database: Env.get("DB_DATABASE", "adonis"),
+      ssl: true,
+    },
+    debug: Env.get("DB_DEBUG", false),
+  },
+};
