6d81026c8f2c388d4f23e2ffc15d93439e2ef0c7
kauer3 <kaue0607@gmail.com>
2021-10-11 13:35:12 -0300

    Add ssl

--- config/database.js
+++ config/database.js
@@ -80,7 +80,9 @@ module.exports = {
       user: Env.get("DB_USER", "root"),
       password: Env.get("DB_PASSWORD", ""),
       database: Env.get("DB_DATABASE", "adonis"),
-      ssl: true,
+      ssl: { 
+        rejectUnauthorized: false 
+      } 
     },
     debug: Env.get("DB_DEBUG", false),
   },
